8ffeb19b50bcadb9a953c58070e571dbce77bf8e
Wone11 <gelayegeremew@gmail.com>
2022-10-11 09:30:28 +0300

    add function added by main branch

---
+++ add.py
@@ -0,0 +1,13 @@
+
+'''
+sum of numbers
+'''
+def Sum(numberOne,numberTwo) :
+    return numberOne + numberTwo
+
+
+#call a methods
+
+if __name__ == '__main__':
+    print('Sum : ', Sum(12,14))
+    
