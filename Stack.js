57bb5f397672a22b34cb5ebcaf9c1bbfc916288f
Wone11 <gelayegeremew@gmail.com>
2022-11-18 21:08:36 +0100

    stack data structure function added and tests done manually

---
+++ Stack.js
@@ -0,0 +1,48 @@
+
+//Creating a stack 
+var Stack  = function(){
+    this.count=0
+    this.storage ={}
+
+    // add values to the stack
+    this.push = function(value){
+        this.storage[this.count] = value;
+        this.count ++;
+    }
+
+    //remove element from the stack 
+    this.pop = function(){
+        if(this.count ===0){
+            return undefined
+        }
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    // the size of the stack 
+    this.size  =function(){
+        return this.count
+    }
+
+    // the pick stack value 
+
+    this.peek = function(){
+        return this.storage[this.count-1]
+    }
+}
+
+// Test manually your work here..
+
+var Test_Stack = new Stack()
+
+Test_Stack.push(1)
+Test_Stack.push(2)
+Test_Stack.push(3)
+Test_Stack.push(8)
+
+console.log(Test_Stack.peek())
+console.log(Test_Stack.size())
+console.log(Test_Stack.pop())
+console.log(Test_Stack.peek())
